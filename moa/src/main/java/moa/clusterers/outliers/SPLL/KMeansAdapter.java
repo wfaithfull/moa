b4f24fb75d7d5e89b225b27be9f2d88d4dddfe19
wfaithfull <willfaithfull89@gmail.com>
2014-11-18 23:07:48 +0000

    Quick and dirty adaptation of kmpp from apache commons. Very sub optimal. Will replace with actual dependency hierarchy when I get around to it.

--- moa/src/main/java/moa/clusterers/outliers/SPLL/KMeansAdapter.java
+++ moa/src/main/java/moa/clusterers/outliers/SPLL/KMeansAdapter.java
@@ -9,27 +9,35 @@ import com.yahoo.labs.samoa.instances.Instances;
 import moa.cluster.Cluster;
 import moa.cluster.Clustering;
 import moa.cluster.InstanceRetainingCluster;
-import moa.cluster.SphereCluster;
-import moa.clusterers.KMeans;
+import moa.clusterers.KMeansPlusPlus;
 
 public class KMeansAdapter implements ClusterProvider {
 
 	@Override
 	public Clustering kMeans(Instances data, int k, int maxIterations) {
 
-		Cluster[] centres = new Cluster[k];
+		KMeansPlusPlus<Instance> kmpp = new KMeansPlusPlus<Instance>(k, maxIterations);
 		
-		for(int i=0;i<k; i++) {
-			centres[i] = new SphereCluster(data.get(i).toDoubleArray(), 1);
-		}
-		
-		List<Cluster> dataAsClusters = new ArrayList<Cluster>();
-		
+		List<Instance> convertedData = new ArrayList<Instance>();
 		for(int i=0;i<data.numInstances();i++) {
-			dataAsClusters.add(new SphereCluster(data.get(i).toDoubleArray(),1));
+			convertedData.add(data.get(i));
 		}
 		
-		return KMeans.kMeans(centres, dataAsClusters);
+		
+		List<InstanceRetainingCluster> clustered = new ArrayList<InstanceRetainingCluster>();
+		Clustering clustering = null;
+		try {
+			clustered = kmpp.cluster(convertedData);
+			clustering = new Clustering();
+			for(InstanceRetainingCluster irc : clustered) {
+				clustering.add((Cluster)irc);
+			}
+		}
+		catch (Exception ex) {
+			System.out.print("Oh no.");
+		}
+		
+		return clustering;
 	}
 	
 	public List<InstanceRetainingCluster> kMeansPP(final List<? extends Instance> data, final int k){
