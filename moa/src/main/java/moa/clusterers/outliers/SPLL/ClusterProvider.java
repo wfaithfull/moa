49083b6dc4db2757e24d7498cb3f2b5c75a9f249
wfaithfull <willfaithfull89@gmail.com>
2014-11-19 01:07:46 +0000

    Changed clusterprovider to return a list of InstanceRetainingCluster

--- moa/src/main/java/moa/clusterers/outliers/SPLL/ClusterProvider.java
+++ moa/src/main/java/moa/clusterers/outliers/SPLL/ClusterProvider.java
@@ -1,14 +1,17 @@
 package moa.clusterers.outliers.SPLL;
 
+import java.util.List;
+
 import com.yahoo.labs.samoa.instances.Instances;
 
 import moa.cluster.Clustering;
+import moa.cluster.InstanceRetainingCluster;
 
 /**
  * Created by Will Faithfull on 14/10/14.
  */
 public interface ClusterProvider {
 
-    Clustering kMeans(Instances data, int k, int maxIterations);
+    List<InstanceRetainingCluster> cluster(Instances data, int k, int maxIterations);
 
 }
