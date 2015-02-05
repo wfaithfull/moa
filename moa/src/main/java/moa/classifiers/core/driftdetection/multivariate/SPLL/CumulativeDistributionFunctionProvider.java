0b7957b9106986b3f8b0690d13a541216e74f808
wfaithfull <willfaithfull89@gmail.com>
2015-02-05 00:28:51 +0000

    Removed the chisquareadapter and relevant interfaces. It has been superceded by the general StatsProvider and ApacheStatsAdapter

--- moa/src/main/java/moa/classifiers/core/driftdetection/multivariate/SPLL/CumulativeDistributionFunctionProvider.java
+++
@@ -1,7 +0,0 @@
-package moa.classifiers.core.driftdetection.multivariate.SPLL;
-
-public interface CumulativeDistributionFunctionProvider {
-	
-	double cumulativeProbability(double x, int df);
-	
-}
