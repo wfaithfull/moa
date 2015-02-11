5f22b94b64e4e3c5545b5324a374f6e55d49659e
wfaithfull <willfaithfull89@gmail.com>
2015-02-11 19:21:34 +0000

    Organized imports

--- moa/src/main/java/moa/classifiers/core/driftdetection/multivariate/SPLL/ClusterProvider.java
+++ moa/src/main/java/moa/classifiers/core/driftdetection/multivariate/SPLL/ClusterProvider.java
@@ -2,10 +2,6 @@ package moa.classifiers.core.driftdetection.multivariate.SPLL;
 
 import java.util.List;
 
-import moa.cluster.InstanceRetainingCluster;
-
-import com.yahoo.labs.samoa.instances.Instances;
-
 /**
  * Created by Will Faithfull on 14/10/14.
  */
