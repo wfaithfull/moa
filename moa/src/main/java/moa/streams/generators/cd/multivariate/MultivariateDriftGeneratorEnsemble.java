a73b553daf6b69e51f9e8b8cac51f400bb83758d
wfaithfull <willfaithfull89@gmail.com>
2015-04-08 16:03:16 +0100

    refactored prepareForUseImpl and added ensemble padding

--- moa/src/main/java/moa/streams/generators/cd/multivariate/MultivariateDriftGeneratorEnsemble.java
+++ moa/src/main/java/moa/streams/generators/cd/multivariate/MultivariateDriftGeneratorEnsemble.java
@@ -20,7 +20,6 @@ public class MultivariateDriftGeneratorEnsemble extends AbstractMultivariateConc
 
 	private static final long serialVersionUID = 2360124636284627056L;
 	private AbstractConceptDriftGenerator[] ensemble;
-	private int dimensions;
 	
 	public ListOption driftGeneratorsOption = new ListOption("generators", 'g',
             "Generator(s) to use.", new ClassOption("generator", 'c',
@@ -30,33 +29,68 @@ public class MultivariateDriftGeneratorEnsemble extends AbstractMultivariateConc
 	
 	@Override
 	protected void prepareForUseImpl(moa.tasks.TaskMonitor monitor, moa.core.ObjectRepository repository) {
+		
+		super.prepareForUseImpl(monitor, repository);
+		
 		Option[] driftGenerators = this.driftGeneratorsOption.getList();
 		
 		// I would like to dynamically update the features option based on the number of generators,
 		// but this is somewhat impractical.
-		if(this.numFeaturesOption.getValue() != driftGenerators.length) {
-			String message = String.format("Generators: [%d]\t Features [%d]", driftGenerators.length, this.numFeaturesOption.getValue());
-			throw new IllegalArgumentException(message + "\nThe number of generators must be equal to the number of features");
-		}
 		
-		ensemble = new AbstractConceptDriftGenerator[driftGenerators.length];
+		AbstractConceptDriftGenerator[] generators = new AbstractConceptDriftGenerator[driftGenerators.length];
 		
 		for(int i=0; i<driftGenerators.length; i++) {
 			// This is rather inelegant, to put it lightly.
-			ensemble[i] = ((AbstractConceptDriftGenerator)((ClassOption)driftGenerators[i]).materializeObject(monitor, repository));
+			
+			//generators[i] = ((AbstractConceptDriftGenerator)(this.getPreparedClassOption((ClassOption)driftGenerators[i]));
+			generators[i] = ((AbstractConceptDriftGenerator)((ClassOption)driftGenerators[i]).materializeObject(monitor, repository));
 			
 			if(monitor.taskShouldAbort()) {
 				return;
 			}
 			
-			if(ensemble[i] instanceof OptionHandler) {
+			if(generators[i] instanceof OptionHandler) {
 				monitor.setCurrentActivity("Preparing drift generator " + (i+1) + "...", -1.0);
 			}
 		}
+		
+		int nFeatures = this.numFeaturesOption.getValue();
+		int nGenerators = generators.length;
+		
+		
+		if(nFeatures < nGenerators) {
+			String message = String.format("Generators: [%d]\t Features [%d]", driftGenerators.length, this.numFeaturesOption.getValue());
+			throw new IllegalArgumentException(message + "\nThere must be at least as many features as specified generators");
+		}
+		else if(nFeatures > nGenerators){
+			ensemble = padEnsemble(generators, nFeatures);
+		}
+		else{
+			ensemble = generators;
+		}
+
+	}
+	
+	private AbstractConceptDriftGenerator[] padEnsemble(AbstractConceptDriftGenerator[] driftGenerators, int nFeatures) {
+		
+		// In the event that we have more requested features than generators, this method pads out the ensemble.
+		AbstractConceptDriftGenerator[] generatorEnsemble = new AbstractConceptDriftGenerator[nFeatures];
+		
+		for(int i=0,j=0;i<nFeatures;i++,j++) {
+			if(j >= driftGenerators.length)
+				j=0;
+			generatorEnsemble[i] = driftGenerators[j];
+			generatorEnsemble[i].prepareForUse();
+		}
+		
+		return generatorEnsemble;
 	}
 	
 	@Override
 	protected double[] nextValues() {
+		
+		int dimensions = this.numFeaturesOption.getValue();
+		
 		double[] values = new double[dimensions];
 		
 		// Grab the values from the respective drift generators
